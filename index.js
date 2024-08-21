66530c8559b968b82c79e68bd4aec0e981964253
Soumya260 <soumya3844@gmail.com>
2024-08-21 07:16:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function teacherlogin(teacher){
+    window.location.href = teacher;
+}
+function studentlogin(student){
+    window.location.href = student;
+}
+function gotohome(home){
+    window.location.href = home;
+}
+function gotosignup(signup){
+    window.location.href = signup;
+
+}
